b675940553c610a3d2e8b58dc1ca1dab9a7d07d8
Gerardo Agustin <gerardoagustin@hotmail.com>
2022-01-25 13:11:34 -0300

    Se modifico el html para no repetirlo en cada pagina

---
+++ template/footer.php
@@ -0,0 +1,55 @@
+
+    <!-- footer section starts -->
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i> home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i> package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i> book</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i> +598 99 760 469</a>
+                <a href="#"><i class="fas fa-phone"></i> +598 99 760 469</a>
+                <a href="#"><i class="fas fa-envelope"></i>gerardoagustin@hotmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>artigas, uruguay - 56000</a>
+            </div>
+
+            <div class="box">
+                <h3>follow us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i> facebook </a>
+                <a href="#"><i class="fab fa-twitter"></i> twitter </a>
+                <a href="#"><i class="fab fa-instagram"></i> instagram </a>
+                <a href="#"><i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit"> created by <span>gerardo agustin</span> | all rights reserved </div>
+
+    </section>
+
+    <!-- footer section ends -->
+
+
+<!-- swiper js file link -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/services.css">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/home-about.css">
+    <link rel="stylesheet" href="css/home-packages.css">
+    <link rel="stylesheet" href="css/home-offer.css">
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/reviews.css">
+    <link rel="stylesheet" href="css/packages.css">
+    <link rel="stylesheet" href="css/book.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/media_queries.css">
+
+</head>
+<body>
+    <!-- header section starts -->
+
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+    <!-- header section ends -->
